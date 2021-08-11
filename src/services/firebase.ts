5c39394531ab5747a3f9cbb980bfce9db5d59dbd
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2021-08-10 22:16:24 -0300

    changing databaseURL

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -6,7 +6,7 @@ import "firebase/database";
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: "https://covid19br-panel-default-rtdb.firebaseio.com",
+  databaseURL: "https://covid19br-database-default-rtdb.firebaseio.com/",
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
